ff7c68871b006ca8148235710d89596fdf122305
Noor_Mohammad <noor.jsdivs@gmail.com>
2022-05-15 02:05:41 +0400

    twitter-clone stated

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Feed from '../components/Feed'
+import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
+
+const Home: NextPage = () => {
+  return (
+    <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
+      <Head>
+        <title>Twitter-clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="grid grid-cols-9">
+        <Sidebar />
+        <Feed />
+        <Widgets />
+      </main>
+    </div>
+  )
+}
+
+export default Home
