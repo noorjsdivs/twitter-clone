071f2f57e749242503c57a07463d90d21aef332e
Noor_Mohammad <noor.jsdivs@gmail.com>
2022-05-19 23:52:20 +0400

    Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,28 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import { Toaster } from 'react-hot-toast'
 import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
+import { Tweet } from '../typings'
+import { fetchTweets } from '../utils/fetchTweets'
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[]
+}
+
+const Home = ({ tweets }: Props) => {
   return (
-    <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
+    <div className="mx-auto  overflow-hidden lg:max-w-6xl">
       <Head>
         <title>Twitter-clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Toaster />
 
       <main className="grid grid-cols-9">
         <Sidebar />
-        <Feed />
+        <Feed tweets={tweets} />
         <Widgets />
       </main>
     </div>
@@ -22,3 +30,12 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const tweets = await fetchTweets()
+  return {
+    props: {
+      tweets,
+    },
+  }
+}
